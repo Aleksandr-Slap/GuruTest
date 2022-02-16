33a3b940e8b41c91dbeb08f52df2b93849bb4a9b
KALi <aleksandr-slap@rambler.ru>
2022-02-16 17:43:44 +0300

    TASK2, application basic settings

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module GuruTest
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.encoding = 'utf-8'
+    config.time_zone = 'Moscow'
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ru
   end
 end
