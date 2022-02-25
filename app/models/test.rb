0690391cb93e1eee8f1db4e1da0229c06e920e11
KALi <aleksandr-slap@rambler.ru>
2022-02-25 19:55:24 +0300

    created method class and instance, filled seeds.rd

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.category(name_category)
+    self.joins('JOIN categories ON categories.id = tests.category_id').where(categories: {title: name_category}).order('tests.title DESC').pluck('tests.title')
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def my_tests(level_test)
+    Test.joins('JOIN tests_users ON tests_users.test_id = tests.id').where(tests: {level: level_test}).where('tests_users.user_id = ?', self.id).pluck('tests.title')
+  end  
 end
