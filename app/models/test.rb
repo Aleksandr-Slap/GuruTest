8d562c75ad6ebab93d65150790e702fddbb911bb
KALi <aleksandr-slap@rambler.ru>
2022-02-28 03:48:10 +0300

    added scope methods and validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,18 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :user 
   belongs_to :category
+  
+  validates :title, presence: true, 
+                    uniqueness: { scope: :level }
+  validates :level, presence: true, 
+                    numericality: { only_integer: true, greater_than: -1 }
+
+  scope :easy, ->{where(level: 0..1)}
+  scope :midl, ->{where(level: 2..4)}
+  scope :hard, ->{where(level: 5..)}
+  scope :show_tests, ->(name_category) { joins(:category).where(categories: { title: name_category }) } 
+
+  def self.category_tests (name_category)
+    show_tests(name_category).order(title: :desc).pluck(:title)
+  end  
 end
